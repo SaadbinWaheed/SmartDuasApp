1c6f5397a598a75a1b076a839b40d84696526afc
Saad <saadbnwhd@gmail.com>
2018-07-15 14:14:25 +0500

    Tested Arabic Text in a simple TextView

--- app/src/main/java/com/example/saad/smartduasapp/Home.java
+++ app/src/main/java/com/example/saad/smartduasapp/Home.java
@@ -1,13 +1,17 @@
 package com.example.saad.smartduasapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    public TextView arabictext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,18 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Give path to the Font location
+        String Path2font = "DroidNaskh-Regular.ttf";
+        // Give label to TextView object defined in layout
+        arabictext = (TextView) findViewById(R.id.arabicphrase);
+
+        // Load the Font and define typeface accordingly
+        Typeface tf = Typeface.createFromAsset(getAssets(), Path2font);
+
+        // Apply the font to your TextView object
+        arabictext.setTypeface(tf);
+        arabictext.setText("احب الاطفال");
 
     }
 
